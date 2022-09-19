0f9d8d939a53d3bc92cf6707a5f18189957d1579
Hannah Brownson <hannahbrownson18@gmail.com>
2022-09-18 23:44:27 -0400

    added tylers css and fixed some more bugs

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -70,4 +70,35 @@ router.get("/edit/:id", withAuth, (req, res) => {
     });
 });
 
+router.get("/create/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    attributes: ["id", "title", "created_at", "post_content"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("create-post", { posts, loggedIn: true });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
